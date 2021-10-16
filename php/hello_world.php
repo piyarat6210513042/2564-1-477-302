5ddf7ec94203c42d963d9708520c57f75ff4d75c
piyarat chumsang <6210513042@psu.ac.th>
2021-10-16 19:21:41 +0700

    php

---
+++ php/hello_world.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head> 
+<body>
+    <?php
+    $txt1 = " I am ";
+    $age = 40;
+    $txt2 = "years old.";
+   /* echo $txt1;
+    echo $age;
+    echo $txt2;
+    */
+    echo $txt1 . $age . $txt2;
+        //echo  "<div style ='color:red'><h1>Hello World in PHP </h1></div> without HTML!"; 
+     ?>
+   
+</body>
+</html>
