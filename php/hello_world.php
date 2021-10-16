448c3bee47b4d2c0ae96a83a22633dc556489f4f
piyarat chumsang <6210513042@psu.ac.th>
2021-10-16 21:55:15 +0700

    php

--- php/hello_world.php
+++ php/hello_world.php
@@ -8,15 +8,22 @@
 </head> 
 <body>
     <?php
-    $txt1 = " I am ";
-    $age = 40;
-    $txt2 = "years old.";
-   /* echo $txt1;
-    echo $age;
-    echo $txt2;
-    */
-    echo $txt1 . $age . $txt2;
-        //echo  "<div style ='color:red'><h1>Hello World in PHP </h1></div> without HTML!"; 
+        $txt1 = " I am ";
+        $age = 40;
+        $txt2 = "years old.";
+        $_txt1="";
+        $chk = true;
+        if($chk){
+            //true
+        }else{
+            //false
+        }
+        /* echo $txt1;
+        echo $age;
+        echo $txt2;
+        */
+        echo $txt1 . $age . $txt2;
+        echo "<div style ='color:red'><h1>Hello World in PHP </h1></div> without HTML!"; 
      ?>
    
 </body>
